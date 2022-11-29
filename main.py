e03f9035137c18190e532bd2bbf534fcc6a8e046
Cristobal <cmoreno.del@gmail.com>
2022-11-29 17:26:02 +0100

    fix: updated how to get SSID list

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import subprocess 
 import platform
 
-from wifiDetect import network
+from wifiDetect import WifiDetector
 
 # string con el path a nuestro .txt con las redes que queremos que busque.
-path_redes_objetivo = "/home/koper/Documentos/GitHub/wifi-alarm/redes.txt"
+file_path = "/home/koper/Documentos/GitHub/wifi-alarm/redes.txt"
 # detectamos el SO desde el que corremos el .py
-launcher = network(platform.system(), )         
+my_os = platform.system()
+launcher = WifiDetector(my_os, file_path)         
 
 
 
--- wifiDetect.py
+++ wifiDetect.py
@@ -1,28 +1,28 @@
 import subprocess
 import pandas as pd
 
-class network:
-    def __init__(self, sistema, path=""):
+class WifiDetector:
+    def __init__(self, my_os, path=""):
         self.network_file = ""
         self.path = path
         self.SSID_list = []
-        self.OS = sistema 
-        self.get_system_networks()
-        self.create_networks_file()
-        self.lista_redes = get_network_list()
+        self.OS = my_os 
+        #self.networks = self.detect_networks()
+        #self.create_networks_file()
+        #self.network_list = self.get_network_list()
 
 
-    def get_system_networks(self):
+    def detect_networks(self):
         """
         Obtiene información de las redes detectadas por el equipo.
         """
         if self.OS == "Linux":
             networks = subprocess.check_output(["nmcli", "dev", "wifi"])
-            self.networks = networks.decode("utf-8", errors="ignore")            
+            networks = networks.decode("utf-8", errors="ignore")            
         else:
             networks = subprocess.check_output(['netsh', 'wlan', 'show', "networks"])
-            self.networks = networks.decode("utf-8", errors="ignore")
-        return self.networks
+            networks = networks.decode("utf-8", errors="ignore")
+        return networks
 
 
     def create_networks_file(self):             # obtener un return path?
@@ -36,6 +36,7 @@ class network:
         self.networks_file.write(networks_clean)
         self.networks_file.close()
 
+
     def get_networks_file(self):
         return self.network_file   # opción para descargar archivo. 
 
@@ -45,43 +46,55 @@ class network:
         Abre el archivo con nuestras redes anotadas para devolver una lista de las redes.
         Si el archivo está vacío, nos avisa de que no hay ninguna red anotada.
         """
-        red_objetivo = open(self.path, "r", encoding="utf-8")
-        self.lista_redes = red_objetivo.read().split()
-        if len(self.lista_redes) < 1:
+        network_target = open(self.path, "r", encoding="utf-8")
+        network_list = network_target.read().split()
+        if len(network_list) < 1:
             print("Atención, no hay ninguna red Wifi anotada en el archivo redes.txt.")
             return None
         else:
-            return self.lista_redes
+            return network_list
 
-    #ACABAR ESTAS LINEAS DE ABAJO.
-    def check_network(self):
-        for red in lista_redes:
-            if red in launcher:
-            print("La red {} se encuentra conectada!".format(red))
+
+    # #ACABAR ESTAS LINEAS DE ABAJO.
+    # def check_network(self):
+    #     for red in lista_redes:
+    #         if red in launcher:
+    #         print("La red {} se encuentra conectada!".format(red))
+
+
+
+
 
 #####
-objetivo = network("Linux")
-datos_wifi = objetivo.get_system_networks()   # devuelve un str.
+file_path = "/home/cmdl987/GitHub/wifi-alarm/redes.txt"
+launcher = WifiDetector("Linux", file_path)    
+wifi_network = launcher.detect_networks()   # devuelve un str.
+lines = wifi_network.splitlines()
+for line in lines:
+    linea = line.split()
+    print(linea[1])
+
+#print(wifi_network)
 #print(datos_wifi)
 
-"""
-archivo_wifi = open("redes_wifi.csv", "w")
-for car in ["*", "IN-USE"]:
-    datos_wifi = datos_wifi.replace(car, " ")
-archivo_wifi.write(datos_wifi)
-archivo_wifi.close()
-"""
-df = pd.read_csv("/home/koper/Documentos/GitHub/wifi-alarm/redes_wifi.csv", 
-                           sep="\s+", index_col=False, 
-                           usecols=[1])
+# """
+# archivo_wifi = open("redes_wifi.csv", "w")
+# for car in ["*", "IN-USE"]:
+#     datos_wifi = datos_wifi.replace(car, " ")
+# archivo_wifi.write(datos_wifi)
+# archivo_wifi.close()
+# """
+# df = pd.read_csv("/home/koper/Documentos/GitHub/wifi-alarm/redes_wifi.csv", 
+#                            sep="\s+", index_col=False, 
+#                            usecols=[1])
 
-print(df)
-"""
-print(df["SSID"][0])
-lista_SSID = [i for i in archivo_wifi["SSID"]]
-print(lista_SSID)
-if "THOM_ONO4601" in lista_SSID:
-    print("True")
-else: 
-    print("False")
-"""
+# print(df)
+# """
+# print(df["SSID"][0])
+# lista_SSID = [i for i in archivo_wifi["SSID"]]
+# print(lista_SSID)
+# if "THOM_ONO4601" in lista_SSID:
+#     print("True")
+# else: 
+#     print("False")
+# """
